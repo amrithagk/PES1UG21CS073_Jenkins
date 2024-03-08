4d92d47d0c3349a9c642ee596a041c329d0837fa
Amritha GK <101631715+amrithagk@users.noreply.github.com>
2024-03-08 18:05:45 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I, PES1UG21CS073, have successfully built and run\n";
+  cout << "Change to trigger build" << endl;
   return 0;
 }
